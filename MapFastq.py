c1f69fa6fbac8f0dfd062f098862e8a1c5910b5f
ndror <>
2017-12-12 21:48:27 +0200

    Fixed issue with cutadapt and Bowtie paths. Added option for running R2 data.
    Signed-off-by: ndror <naamadror@gmail.com>

--- MapFastq.py
+++ MapFastq.py
@@ -2,12 +2,32 @@ import os
 from subprocess import PIPE, Popen
 import Shared
 
-CutAdaptCMD = '/usr/local/bin/cutadapt'
+usage = '''MapFastq.py  
+   -o  --out-dir            [str]   Output directory. Defaults to current directory if left unspecified.
+   -i  --input-fastq-file   [str]   REQUIRED. Input fastq file (Need to include path and '.fastq.gz' at end of filename)
+   -t  --transposon-seq     [str]   Transposon sequence, default is Berman Tn with primer
+   -a  --clean-adapters     [bool]  Clean Illumina universl adapters. Default is False
+   -d  --delete-originals   [bool]  Delete input FASTQ files. Default is False
+   -k  --keep-clean-fqs     [bool]  Keep the cleaned FASTQ files. Default is False
+   -r  --run-reverse        [bool]  Run program for reverse read (R2) sequence data. Default is False
+   -h  --help                       Show this help message and exit 
+'''
+
 TnPrimerAndTail = 'GTATTTTACCGACCGTTACCGACCGTTTTCATCCCTA'
-BowtiePath = '/usr/local/bin/'
+AdapterSeq = 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC'
+
+RevTn = 'TAGGGATGAAAACGGTCGGTAACGGTCGGTAAAATAC'
+RevAdapter = 'GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT'
+
 # NB: bowtie2 requires spaces to be escapes with a backslash for the -x parameter.
 CInd = Shared.get_dependency("albicans", "reference genome", "C_albicans_SC5314_version_A22-s07-m01-r08_chromosomes_HapA").replace(' ', '\ ')
 CORES = 4 # How many cores on the machine = how many threads should the external tools utilize
+
+CutadaptShellPath = os.popen('command -v cutadapt').read()
+CutAdaptPath = CutadaptShellPath.strip('\n')
+
+BowtieShellPath = os.popen('command -v bowtie2').read()
+BowtiePath = BowtieShellPath.strip('\n')
 
 def GetReads(Val,Text):
     Sub = Text[Text.find(Val)+len(Val)+1:].lstrip(' ')
@@ -33,20 +53,10 @@ def cmdline(command):
 def RemoveTn(InputFileName, TnSeq, overlap, OutputFName):
     #-g is from the beggining and -a is from the end or vice versa (if we use -a we get only a string length 6, which is probably the 6 Ns between the primer and Tn)
     #-o out put, afterwards the input. --overlap determines the minimal number of bases found from the Tn
-    cmdl = CutAdaptCMD + ' -m 2 -g ' + TnSeq + ' -o "'+ OutputFName + '" "' + InputFileName + '" --discard-untrimmed --overlap ' + str(overlap)
-    Output,err = cmdline(cmdl)
+    CutAdaptCmd = CutAdaptPath + ' -m 2 -g ' + TnSeq + ' -o "'+ OutputFName + '" "' + InputFileName + '" --discard-untrimmed --overlap ' + str(overlap)
+    Output,err = cmdline(CutAdaptCmd)
     return ProcessOutput(Output)
     
-    
-
-usage = """USAGE: MapFastq.py  
-   -o  --OutDir          [str]   Output fir, if not specified creating all file in curr dir.
-   -i  --InputFileName   [str]   Input fastq file name 
-   -a  --CleanAdapters           Clean Illumina universl adapters.
-   -d  --DeleteOriginals         Delete input FASTQ files.
-   -k  --KeepCleanFqs            Keep the cleaned FASTQ files. 
-   -h, --help                    print this help 
-"""
 
 if __name__ == '__main__':
     import argparse
@@ -54,9 +64,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--out-dir", default='.')
     parser.add_argument("-i", "--input-file-name", required=True)
-    parser.add_argument("-a", "--clean-adapters", default=False, action='store_true')
+    parser.add_argument("-a", "--clean-adapters", default=True, action='store_true')
     parser.add_argument("-d", "--delete-originals", default=False, action='store_true')
     parser.add_argument("-k", "--keep-clean-fastqs", default=False, action='store_true')
+    parser.add_argument("-r", "--run-reverse", default=False, action='store_true')
     
     args = parser.parse_args()
     
@@ -65,6 +76,12 @@ if __name__ == '__main__':
     clean_adapters = args.clean_adapters
     delete_originals = args.delete_originals
     keep_clean_fqs = args.keep_clean_fastqs
+    run_reverse = args.run_reverse
+    
+    # setting sequennces to run on reverse read sequence file
+    if run_reverse:
+        TnPrimerAndTail = RevTn
+        AdapterSeq = RevAdapter
     
     # NB: we're first removing the transposon head from the beginning and then 
     # removing the adapter. This is because the sequencing tech, for whatever reason,
@@ -73,16 +90,18 @@ if __name__ == '__main__':
     # After we have all of the reads that have a transposon, we then trim the adapter from the tail.
     CleanfName = os.path.join(OutputDir, os.path.basename(FastqFName) + '.clean.fq')
     CurrRes = RemoveTn(FastqFName,TnPrimerAndTail,37, CleanfName)
+    
     if clean_adapters:
         temp_fq = CleanfName + ".no_adapters.fq"
-        cmdline(r'''%s -m 2 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -o "%s" "%s"''' %
-                (CutAdaptCMD, temp_fq, CleanfName))
+        cmdline(r'''%s -m 2 -a %s -o "%s" "%s"''' %
+                (CutAdaptPath, AdapterSeq, temp_fq, CleanfName))
         os.unlink(CleanfName)
         os.rename(temp_fq, CleanfName)
+    
     #aligning fastq file to the reference genome
     #-x reference_genome; -U fq file of unpaired reads; -S output SAM alignment file; X is the max fragment size to consider (relevant only in paired end)
     SamfName = os.path.join(OutputDir, os.path.splitext(os.path.basename(FastqFName))[0] + '.sam')
-    BowtieCmd = BowtiePath + 'bowtie2 -p ' + str(CORES) + ' -x "'+ CInd + \
+    BowtieCmd = BowtiePath + ' -p ' + str(CORES) + ' -x "'+ CInd + \
                 '" -U "' + CleanfName + '" -S "' + SamfName + '"'
     Output,err = cmdline(BowtieCmd)
     # converting to bam file 
