cee79d2d737334165f65dc9cc80e66f9c9953f8d
ndror <>
2017-12-14 00:17:38 +0200

    Added terminal output for cutadapt functions. Removed non-functional "run reverse". Terminal output also saves to 'log.txt' file.
    Signed-off-by: ndror

--- MapFastq.py
+++ MapFastq.py
@@ -6,28 +6,23 @@ usage = '''MapFastq.py
    -o  --out-dir            [str]   Output directory. Defaults to current directory if left unspecified.
    -i  --input-fastq-file   [str]   REQUIRED. Input fastq file (Need to include path and '.fastq.gz' at end of filename)
    -t  --transposon-seq     [str]   Transposon sequence, default is Berman Tn with primer
-   -a  --clean-adapters     [bool]  Clean Illumina universl adapters. Default is False
-   -d  --delete-originals   [bool]  Delete input FASTQ files. Default is False
-   -k  --keep-clean-fqs     [bool]  Keep the cleaned FASTQ files. Default is False
-   -r  --run-reverse        [bool]  Run program for reverse read (R2) sequence data. Default is False
+   -a  --clean-adapters     [bool]  Clean Illumina universl adapters.
+   -d  --delete-originals   [bool]  Delete input FASTQ files.
+   -k  --keep-clean-fqs     [bool]  Keep the cleaned FASTQ files.
    -h  --help                       Show this help message and exit 
 '''
 
 TnPrimerAndTail = 'GTATTTTACCGACCGTTACCGACCGTTTTCATCCCTA'
 AdapterSeq = 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC'
 
-RevTn = 'TAGGGATGAAAACGGTCGGTAACGGTCGGTAAAATAC'
-RevAdapter = 'GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT'
-
 # NB: bowtie2 requires spaces to be escapes with a backslash for the -x parameter.
 CInd = Shared.get_dependency("albicans", "reference genome", "C_albicans_SC5314_version_A22-s07-m01-r08_chromosomes_HapA").replace(' ', '\ ')
 CORES = 4 # How many cores on the machine = how many threads should the external tools utilize
 
-CutadaptShellPath = os.popen('command -v cutadapt').read()
-CutAdaptPath = CutadaptShellPath.strip('\n')
-
-BowtieShellPath = os.popen('command -v bowtie2').read()
-BowtiePath = BowtieShellPath.strip('\n')
+def GetCmdPath(Program):
+    ShellPath = os.popen('command -v ' + Program).read()
+    CmdPath = ShellPath.strip('\n')
+    return CmdPath
 
 def GetReads(Val,Text):
     Sub = Text[Text.find(Val)+len(Val)+1:].lstrip(' ')
@@ -35,12 +30,13 @@ def GetReads(Val,Text):
     Vals = Sub.split(' ')
     return int(Vals[0].replace(',',''))
     
-def ProcessOutput(output):
+def CutAdaptOutput(output):
     Summary = output[output.find('=== Summary ===')+17:]
-    TotalReads = GetReads('Total read pairs processed',Summary)
-    Adapter = GetReads('Reads with adapters',Summary)
-    Total = GetReads('Reads written (passing filters)',Summary)
-    return TotalReads, Adapter, Total
+    TotalRead = GetReads('Total read pairs processed',Summary)
+    TotalWrite = GetReads('Reads written (passing filters)',Summary)
+    Percentage = float(TotalWrite)/float(TotalRead)*100
+    Percent = round(Percentage, 2)
+    return TotalRead, TotalWrite, Percent
 
 def cmdline(command):
     process = Popen(
@@ -50,24 +46,36 @@ def cmdline(command):
     )
     return process.communicate()
 
-def RemoveTn(InputFileName, TnSeq, overlap, OutputFName):
+def RemoveTn(InputFName, TnSeq, overlap, OutputFName):
     #-g is from the beggining and -a is from the end or vice versa (if we use -a we get only a string length 6, which is probably the 6 Ns between the primer and Tn)
     #-o out put, afterwards the input. --overlap determines the minimal number of bases found from the Tn
-    CutAdaptCmd = CutAdaptPath + ' -m 2 -g ' + TnSeq + ' -o "'+ OutputFName + '" "' + InputFileName + '" --discard-untrimmed --overlap ' + str(overlap)
+    CutAdaptCmd = CutAdaptPath + ' -m 2 -g ' + TnSeq + ' -o "'+ OutputFName + '" "' + InputFName + '" --discard-untrimmed --overlap ' + str(overlap)
     Output,err = cmdline(CutAdaptCmd)
-    return ProcessOutput(Output)
+    return CutAdaptOutput(Output)
+
+def AlignFastq(BowtiePath, CleanfName, SamfName):
+    #aligning fastq file to the reference genome
+    #-x reference_genome; -U fq file of unpaired reads; -S output SAM alignment file; X is the max fragment size to consider (relevant only in paired end)
+    BowtieCmd = BowtiePath + ' -p ' + str(CORES) + ' -x "'+ CInd + \
+                '" -U "' + CleanfName + '" -S "' + SamfName + '" 2> temp'
+    Output,err = cmdline(BowtieCmd)
+    temp = open('temp', 'r')
+    temp = temp.read()
+    Log = '\r\n\r\n=== Sequence alignment ===\r\n' + temp.replace('\n', '\r\n')
+    LogFile.write(Log)
+    os.remove('temp')
     
 
+    
 if __name__ == '__main__':
     import argparse
     
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--out-dir", default='.')
     parser.add_argument("-i", "--input-file-name", required=True)
-    parser.add_argument("-a", "--clean-adapters", default=True, action='store_true')
+    parser.add_argument("-a", "--clean-adapters", default=False, action='store_true')
     parser.add_argument("-d", "--delete-originals", default=False, action='store_true')
     parser.add_argument("-k", "--keep-clean-fastqs", default=False, action='store_true')
-    parser.add_argument("-r", "--run-reverse", default=False, action='store_true')
     
     args = parser.parse_args()
     
@@ -76,42 +84,49 @@ if __name__ == '__main__':
     clean_adapters = args.clean_adapters
     delete_originals = args.delete_originals
     keep_clean_fqs = args.keep_clean_fastqs
-    run_reverse = args.run_reverse
     
-    # setting sequennces to run on reverse read sequence file
-    if run_reverse:
-        TnPrimerAndTail = RevTn
-        AdapterSeq = RevAdapter
-    
+    LogFile = open('log.txt', 'r+')
+    CutAdaptPath = GetCmdPath('cutadapt') 
+    BowtiePath = GetCmdPath('bowtie2')
+  
     # NB: we're first removing the transposon head from the beginning and then 
     # removing the adapter. This is because the sequencing tech, for whatever reason,
     # removes the adapater from the 5' Tn end, but keeps the tail adapters (sometimes),
     # presumably if the reads are too short.
     # After we have all of the reads that have a transposon, we then trim the adapter from the tail.
-    CleanfName = os.path.join(OutputDir, os.path.basename(FastqFName) + '.clean.fq')
-    CurrRes = RemoveTn(FastqFName,TnPrimerAndTail,37, CleanfName)
+    CleanfName = os.path.join(OutputDir, os.path.basename(FastqFName) + '.clean.fq')    
+    CurrRes = RemoveTn(FastqFName, TnPrimerAndTail, 37, CleanfName)
+    Log = '=== Transposon removal ===\r\n%s reads; of these:\r\n  %s (%s%%) contained the transposon' % (CurrRes[0], CurrRes[1], CurrRes[2])
+    LogFile.write(Log)
     
     if clean_adapters:
         temp_fq = CleanfName + ".no_adapters.fq"
-        cmdline(r'''%s -m 2 -a %s -o "%s" "%s"''' %
+        Output,err = cmdline(r'''%s -m 2 -a %s -o "%s" "%s"''' %
                 (CutAdaptPath, AdapterSeq, temp_fq, CleanfName))
+        CurrRes = CutAdaptOutput(Output)
+        Log = '\r\n\r\n=== Adapter removal ===\r\n%s reads; of these:\r\n  %s (%s%%) contained the adapter' % (CurrRes[0], CurrRes[1], CurrRes[2])
+        LogFile.write(Log)
         os.unlink(CleanfName)
         os.rename(temp_fq, CleanfName)
-    
+
     #aligning fastq file to the reference genome
-    #-x reference_genome; -U fq file of unpaired reads; -S output SAM alignment file; X is the max fragment size to consider (relevant only in paired end)
+    #-x reference_genome; -U fq file of unpaired reads; -S output SAM alignment file; X is the max fragment size to consider (relevant only in paired end)        
     SamfName = os.path.join(OutputDir, os.path.splitext(os.path.basename(FastqFName))[0] + '.sam')
-    BowtieCmd = BowtiePath + ' -p ' + str(CORES) + ' -x "'+ CInd + \
-                '" -U "' + CleanfName + '" -S "' + SamfName + '"'
-    Output,err = cmdline(BowtieCmd)
+    AlignFastq(BowtiePath, CleanfName, SamfName)
+    
     # converting to bam file 
     cmdline('samtools view -@ ' + str(CORES) + ' -bS "'+ SamfName + '" > "' +
             os.path.splitext(SamfName)[0] + '.bam"')
+            
     #sorting and indexing 
     cmdline('samtools sort -@ ' + str(CORES) + ' "' +
             os.path.splitext(SamfName)[0] + '.bam" > "' +
             os.path.splitext(SamfName)[0] + '.sorted.bam"')
     cmdline('samtools index "' + os.path.splitext(SamfName)[0] + '.sorted.bam"')
+    
+    LogFile.seek(0)
+    print LogFile.read()
+    LogFile.close()
 
     # Remove intermediats:
     if delete_originals:
